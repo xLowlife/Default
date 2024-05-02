5388d2100510af01f92040ec6cb1d429925f3066
xLowlife <168694933+xLowlife@users.noreply.github.com>
2024-05-01 20:42:28 -0700

    Create classTemplate.cxx
    Signed-off-by: xLowlife <168694933+xLowlife@users.noreply.github.com>

---
+++ C++/New Project/classTemplate.cxx
@@ -0,0 +1,34 @@
+/******************************************************************************
+Template
+
+Lowlife
+C++
+
+Purpose of the program:
+
+
+Specification Summary:
+
+
+Sources:
+
+
+"If you don't specify, I may ask you if I can share it."
+*******************************************************************************/
+
+/******************************************************************************
+ *                              Class Template .cxx                           *
+ ******************************************************************************/
+#include "classTemplate.h"
+
+// Function for classTemplate
+// Accepts no Parameters
+// Returns Void, Passes Data by Member Access
+void classTemplate::classTemplateFunction()
+{
+	// Initialize Variable(s) for classTemplateFunction()
+	
+	
+	// Return Void
+	return;
+}
