4c299df98d8b6fbc28cebf6b97073a9f4e9c7d9c
xLowlife <168694933+xLowlife@users.noreply.github.com>
2024-05-01 20:35:19 -0700

    Create classTemplate.h
    Signed-off-by: xLowlife <168694933+xLowlife@users.noreply.github.com>

---
+++ C++/New Project/classTemplate.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+Template
+
+Lowlife
+C++
+
+Purpose of the program:
+
+
+Specification Summary:
+
+
+Sources:
+
+
+"If you don't specify, I may ask you if I can share it."
+*******************************************************************************/
+
+/******************************************************************************
+ *                              Class Template .h                             *
+ ******************************************************************************/
+#ifndef CLASS_TEMPLATE_H
+#define CLASS_TEMPLATE_H
+
+#include <stdlib.h>
+#include <cctype> // isdigit()
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random> // Mersenne Twister
+#include <string>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+const int
+	SIZE{1}; // Specify Size of Template
+
+class classTemplate
+{
+public:
+	// Constructor with Default Parameters
+	classTemplate()
+	{
+	}
+
+	// Destructor with Default Parameters
+	~classTemplate()
+	{
+	}
+
+	// classTemplate Functions
+
+private:
+	// Initialize Variable(s) for classTemplate
+};
+
+#endif
