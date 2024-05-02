6d0406a08310aedc0d0b987a23f536325c946fd7
xLowlife <168694933+xLowlife@users.noreply.github.com>
2024-05-01 19:53:18 -0700

    Create NewProject.cpp
    Signed-off-by: xLowlife <168694933+xLowlife@users.noreply.github.com>

---
+++ C++/New Project/NewProject.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+New Project
+
+Lowlife
+C++
+
+Purpose of the program:
+
+Specification Summary:
+
+Sources:
+
+"If you don't specify, I may ask you if I can share it."
+*******************************************************************************/
+
+/******************************************************************************
+ *                                New Project .cpp                            *
+ ******************************************************************************/
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+  
+  // Return 0
+	return 0;
+}
