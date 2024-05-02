2838c697d78d14aca7c23b0816ccbe6169fd46a6
xLowlife <168694933+xLowlife@users.noreply.github.com>
2024-05-01 20:21:56 -0700

    Update and rename NewProject.cpp to Template.cpp
    Signed-off-by: xLowlife <168694933+xLowlife@users.noreply.github.com>

--- C++/New Project/Template.cpp
+++ C++/New Project/Template.cpp
@@ -1,29 +1,34 @@
 /******************************************************************************
-New Project
+Template
 
 Lowlife
 C++
 
 Purpose of the program:
 
+
 Specification Summary:
 
+
 Sources:
+
 
 "If you don't specify, I may ask you if I can share it."
 *******************************************************************************/
 
 /******************************************************************************
- *                                New Project .cpp                            *
+ *                                 Template .cpp                              *
  ******************************************************************************/
+#include "classTemplate.h"
+#include "classTemplate.cxx"
 #include <stdlib.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	classTemplate Template;
 	
-  
-  // Return 0
+	// Return 0
 	return 0;
 }
